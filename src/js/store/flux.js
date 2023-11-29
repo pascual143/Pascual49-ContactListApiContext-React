7b8e76fbf0e40a0a6a8981b033c79bcb478bd8e6
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2023-11-29 11:35:21 +0000

    adding contactCard to home

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,95 +1,123 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			/* YOUR "GLOBAL" STATES HERE! */
-			agendas: [],
-			currentUser: "",
-			contacts: [],
-		},
-		actions: {
-			// Get all users (agenda_slugs).
-			loadAgendas: () => {
-				fetch("https://playground.4geeks.com/apis/fake/contact/agenda")
-				.then(response => response.json())
-				.then(users => setStore( { "agendas": users } ));
-			},
+  return {
+    store: {
+      /* YOUR "GLOBAL" STATES HERE! */
+      agendas: [],
+      currentUser: "",
+      contacts: [],
+    },
+    actions: {
+      // Get all users (agenda_slugs).
+      loadAgendas: () => {
+        try {
+          fetch("https://playground.4geeks.com/apis/fake/contact/agenda")
+            .then((response) => response.json())
+            .then((users) => setStore({ agendas: users }));
+        } catch (error) {
+          console.error(error);
+        }
+      },
 
-			setUser: (user) => {
-				setStore( {"currentUser": user} );
-				getActions().loadContacts(user)
-			},
+      setUser: (user) => {
+        setStore({ currentUser: user });
+        getActions().loadContacts(user);
+      },
 
-			// Load contacts for current user.
-			loadContacts: (currentUser) => {
-				fetch(`https://playground.4geeks.com/apis/fake/contact/agenda/${currentUser}`)
-				.then(response => response.json())
-				.then(contactList => setStore(  {"contacts": [...contactList]}  ));
-			},
+      // Load contacts for current user.
+      loadContacts: (currentUser) => {
+        try {
+          fetch(
+            `https://playground.4geeks.com/apis/fake/contact/agenda/${currentUser}`
+          )
+            .then((response) => response.json())
+            .then((contactList) => setStore({ contacts: [...contactList] }));
+        } catch (error) {
+          console.log(error);
+        }
+      },
 
-			// Add new contact to currentUser's contacts.
-			addContact: (newContact) => {
-				fetch(`https://playground.4geeks.com/apis/fake/contact`, {
-					method: "POST",
-					body: JSON.stringify({
-						"full_name": newContact.fullName,
-						"email": newContact.email,
-						"agenda_slug": newContact.user,
-						"address": newContact.address,
-						"phone": newContact.phone
-					}),
+      // Add new contact to currentUser's contacts.
+      addContact: (newContact) => {
+        try {
+          fetch(`https://playground.4geeks.com/apis/fake/contact`, {
+            method: "POST",
+            body: JSON.stringify({
+              full_name: newContact.fullName,
+              email: newContact.email,
+              agenda_slug: newContact.user,
+              address: newContact.address,
+              phone: newContact.phone,
+            }),
 
-					headers: {
-						"Content-Type": "application/json"
-					}
-				})
-				.then(response => response.json())
-				.then(data => {
-					getActions().loadContacts(getStore().currentUser)
-				})
-			},
+            headers: {
+              "Content-Type": "application/json",
+            },
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              getActions().loadContacts(getStore().currentUser);
+            });
+        } catch (error) {
+          console.log(error);
+        }
+      },
 
-			// Delete a contact from user's list.
-			deleteContact: (contactId) => {
-				fetch(`https://playground.4geeks.com/apis/fake/contact/${contactId}`, {
-					method: "DELETE",
-					headers: {
-						"Content-Type": "application/json"
-					}
-				})
-				.then(response => response.json())
-				.then(data => {
-					getActions().loadContacts(getStore().currentUser)
-				})				
-			},
+      // Delete a contact from user's list.
+      deleteContact: (contactId) => {
+        try {
+          fetch(
+            `https://playground.4geeks.com/apis/fake/contact/${contactId}`,
+            {
+              method: "DELETE",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          )
+            .then((response) => response.json())
+            .then((data) => {
+              getActions().loadContacts(getStore().currentUser);
+            });
+        } catch (error) {
+          console.log(error);
+        }
+      },
 
-			// Edit a contact from user's list.
-			editContact: (contact) => {
-				fetch(`https://playground.4geeks.com/apis/fake/contact/${contact.id}`, {
-					method: "PUT",
-					body: JSON.stringify({
-						"full_name": contact.fullName,
-						"email": contact.email,
-						"agenda_slug": contact.user,
-						"address": contact.address,
-						"phone": contact.phone
-					}),
+      // Edit a contact from user's list.
+      editContact: (contact) => {
+        try {
+          fetch(
+            `https://playground.4geeks.com/apis/fake/contact/${contact.id}`,
+            {
+              method: "PUT",
+              body: JSON.stringify({
+                full_name: contact.fullName,
+                email: contact.email,
+                agenda_slug: contact.user,
+                address: contact.address,
+                phone: contact.phone,
+              }),
 
-					headers: {
-						"Content-Type": "application/json"
-					}
-				})
-				.then(response => response.json())
-				.then(data => {
-					getActions().loadContacts(getStore().currentUser)
-				})				
-			},
-			selectAgenda:(el)=>{
-				setStore({agenda:el})
-				let { agenda } = getStore()
-				console.log("LA AGENDA ES" + agenda )
-			  },
-		}
-	};
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          )
+            .then((response) => response.json())
+            .then((data) => {
+              getActions().loadContacts(getStore().currentUser);
+            });
+        } catch (error) {
+          console.log(error);
+        }
+      },
+      selectAgenda: (el) => {
+        setStore({ agenda: el });
+        let { agenda } = getStore();
+        console.log("LA AGENDA ES" + agenda);
+      },
+    },
+  };
 };
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,14 +4,11 @@ import { Link } from "react-router-dom";
 import { Navbar } from "../component/navbar";
 
 import ProfilePic from "../../img/profile.jpg";
+import { ContactCard } from "../component/contactCard";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
   const [agenda, setAgenda] = useState("");
-  const [name, setName] = useState();
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [address, setAddress] = useState("");
 
   const contact = store.contacts;
 
@@ -20,26 +17,6 @@ export const Home = () => {
     //console.log(store.agendas);
   }, [agenda, contact]);
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    if (name === "name") {
-      setName(value);
-    } else if (name === "email") {
-      setEmail(value);
-    } else if (name === "phone") {
-      setPhone(value);
-    } else if (name === "address") {
-      setAddress(value);
-    } else if (name === "agenda") {
-      setAgenda(value);
-    }
-  };
-  const handleKeyPress = (e) => {
-    if (e.key === "Enter") {
-      e.preventDefault();
-      editContact();
-    }
-  };
   function selectAgenda(e) {
     setAgenda(e);
     actions.selectAgenda(e);
@@ -96,11 +73,13 @@ export const Home = () => {
                     GET CONTACTS
                   </button>
                 </Link>
+                {/* <!-- 
                 <Link to="/agendas">
                   <button className="btn btn-success mt-3 mx-2">
                     GET AGENDAS
                   </button>
                 </Link>
+                 --> */}
               </div>
             </div>
 
@@ -170,166 +149,14 @@ export const Home = () => {
         </nav>
 
         {/* <!-- Show list of contacts --> */}
-        {store.agendas.length === 0 ? (
-          <p>No Contacts in Agenda.</p>
-        ) : store.agenda === "NOT_FOUND" ? (
-          <p>No Contacts in Agenda.</p>
+        {store.agendas.includes(store.currentUser) ? (
+          store.contacts.map((item, idx) => {
+            return <ContactCard key={idx} info={item} />;
+          })
         ) : (
-          contact.map((el, index) => (
-            <div key={el.id} className="container ">
-              <div className="row align-items-center border border-dark mt-4 rounded p-2">
-                <div className="col-md-2 col-4 d-flex align-items-center justify-content-center">
-                  <img
-                    className="photoprofile rounded-circle"
-                    src={ProfilePic}
-                    alt="Photo"
-                    style={{ width: "100px", height: "100px" }}
-                  />
-                </div>
-                <div className="col-md-7 col-8  d-flex justify-content-start mt-4 mt-0">
-                  <div className="text-start">
-                    <Link to={`/contacts`}>
-                      <h5 className="mb-0">{el.full_name}</h5>
-                    </Link>
-                    <p className="mb-0">Dirección: {el.address}</p>
-                    <p className="mb-0">Teléfono: {el.phone}</p>
-                    <p className="mb-0">Correo electrónico: {el.email}</p>
-                    <p className="mb-0">id: {el.id}</p>
-                  </div>
-                </div>
-                <div className="col d-flex align-items-center justify-content-end mt-3 mt-md-0">
-                  {/* <!-- Button trigger modal Editar --> */}
-                  <button
-                    type="button"
-                    className="btn btn-primary m-2"
-                    data-bs-toggle="modal"
-                    data-bs-target="#ModalEditar"
-                  >
-                    edit
-                  </button>
-                  <button
-                    className="delete btn btn-danger"
-                    onClick={() => deleteitem(el.id)}
-                  >
-                    Eliminar
-                  </button>
-                  {/*  <!-- Modal Editar--> */}
-                  <div
-                    className="modal fade"
-                    id="ModalEditar"
-                    tabIndex="-1"
-                    aria-labelledby="exampleModalLabel"
-                    aria-hidden="true"
-                  >
-                    <div className="modal-dialog">
-                      <div className="modal-content">
-                        <div className="modal-header">
-                          <h1
-                            className="modal-title fs-5"
-                            id="exampleModalLabel"
-                          >
-                            Edit Contact
-                          </h1>
-                          <button
-                            type="button"
-                            className="btn-close"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                          ></button>
-                        </div>
-                        <div className="modal-body">
-                          <form>
-                            <div className="mb-3">
-                              <label htmlFor="fullName" className="form-label">
-                                Full Name
-                              </label>
-                              <input
-                                type="text"
-                                name="name"
-                                value={name}
-                                onChange={handleInputChange}
-                                className="form-control"
-                                id="fullName"
-                                aria-describedby="fullNameHelp"
-                                onKeyDown={handleKeyPress}
-                              />
-                            </div>
-                            <div className="mb-3">
-                              <label htmlFor="email" className="form-label">
-                                Email
-                              </label>
-                              <input
-                                type="email"
-                                name="email"
-                                value={email}
-                                onChange={handleInputChange}
-                                className="form-control"
-                                id="email"
-                                aria-describedby="emailHelp"
-                                onKeyDown={handleKeyPress}
-                              />
-                              <div id="emailHelp" className="form-text">
-                                We'll never share your email with anyone else.
-                              </div>
-                            </div>
-                            <div className="mb-3">
-                              <label htmlFor="phone" className="form-label">
-                                Phone
-                              </label>
-                              <input
-                                type="tel"
-                                name="phone"
-                                value={phone}
-                                onChange={handleInputChange}
-                                className="form-control"
-                                id="phone"
-                                onKeyDown={handleKeyPress}
-                              />
-                            </div>
-                            <div className="mb-3">
-                              <label htmlFor="address" className="form-label">
-                                Address
-                              </label>
-                              <input
-                                type="text"
-                                name="address"
-                                value={address}
-                                onChange={handleInputChange}
-                                className="form-control"
-                                id="address"
-                                onKeyDown={handleKeyPress}
-                              />
-                            </div>
-                            <div className="mb-3">
-                              <label htmlFor="agenda" className="form-label">
-                                Agenda
-                              </label>
-                              <input
-                                type="text"
-                                name="agenda"
-                                value={agenda}
-                                onChange={handleInputChange}
-                                className="form-control"
-                                id="agenda"
-                                onKeyDown={handleKeyPress}
-                              />
-                            </div>
-                            <button
-                              type="button"
-                              onClick={() => editContact(el.id)}
-                              className="btn btn-primary"
-                            >
-                              Submit
-                            </button>
-                          </form>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))
+          <h3>
+            Nothing to display! Select a valid user to display their contacts.
+          </h3>
         )}
       </>
       <></>
